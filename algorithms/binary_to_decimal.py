44484d00aea2427bc5456f7ba170e0045a1e67c9
surenderthakran <surender.thakran89@gmail.com>
2018-05-12 20:13:00 +0530

    updated binary to decimal conversion algo to read binary input from the command line

--- algorithms/binary_to_decimal.py
+++ algorithms/binary_to_decimal.py
@@ -1,26 +1,52 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python2
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-def convertInteger(binary):
+import sys
+
+class InvalidInputException(Exception):
+  """Custom exception class for invalid inputs to the program."""
+
+def convertInteger(binary_input):
   """Converts a integer's representation from binary to decimal.
 
   args:
-    binary: integer in binary as an int data type.
+    binary_input: integer in binary as an int data type.
 
   returns:
     decimal representation of the integer.
   """
   decimal = 0
   position = 0
-  while binary != 0:
-    num = binary % 10
+  while binary_input != 0:
+    # get least significant digit
+    num = binary_input % 10
+
+    if num > 1:
+      raise ValueError('Invalid input binary number:', binary_input)
+
+    # calculate decimal value of the binary digit
     val = num * 2**position
     decimal += val
-    binary = binary/10
+
+    # remove least significant bit by floor division
+    binary_input = binary_input//10
+
+    # increment position by 1
     position += 1
+
   return decimal
 
-print(convertInteger(110110))
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    raise InvalidInputException('No binary input found')
+
+  try:
+    binary = int(sys.argv[1])
+  except ValueError:
+    raise InvalidInputException('Invalid binary input:', sys.argv[1])
+
+  print(convertInteger(int(sys.argv[1])))
