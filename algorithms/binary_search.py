ffc179ebfb5323e0a112212a1c3a4c85b24e47a0
surenderthakran <surender.thakran89@gmail.com>
2018-05-24 17:54:57 +0530

    started binary search algorithm

---
+++ algorithms/binary_search.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8  -*-
+
+"""Find position of an element in a sorted array using binary search.
+
+Usage:
+$ python binary_search.py
+"""
+
+if __name__ == '__main__':
+  arr = [1, 3, 5, 7, 9, 11, 13, 15]
+  print(binary_search(arr, 13))
