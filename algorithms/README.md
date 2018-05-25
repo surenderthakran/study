cdfaf9f954cb5cd7c19303a3c3b3cbc049d2bc54
surenderthakran <surender.thakran89@gmail.com>
2018-05-25 13:37:29 +0530

    updated binary search algorithm

--- algorithms/README.md
+++ algorithms/README.md
@@ -2,3 +2,4 @@
 
 ### 1. [Binary To Decimal](binary_to_decimal.py)
 ### 2. [Decimal To Binary](decimal_to_binary.py)
+### 3. [Binary Search](binary_search.py)
--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -7,6 +7,37 @@ Usage:
 $ python binary_search.py
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def binary_search(arr, elem):
+  """Return index of the given element in the list.
+
+  If the element doesn't exists in the list return -1.
+
+  Args:
+    arr: list of integers.
+    elem: integer element to find in the array.
+
+  Returns:
+    integer position of the element or -1 if the element doesn't exists.
+  """
+  low = 0
+  high = len(arr) - 1
+
+  while high >= low:
+    mid = low + (high - low)//2
+    if elem == arr[mid]:
+      return mid
+    elif elem < arr[mid]:
+      high = mid - 1
+    else:
+      low = mid + 1
+
+  return -1
+
 if __name__ == '__main__':
   arr = [1, 3, 5, 7, 9, 11, 13, 15]
-  print(binary_search(arr, 13))
+  print(binary_search(arr, 5))
