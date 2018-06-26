11a88ea5cccb0b00628719207c6a6ee5f17687d2
surenderthakran <surender.thakran89@gmail.com>
2018-06-26 15:12:38 +0530

    Updated binary serach tree implementation to add method for inorder traversal and method to create a binary search tree from a given preorder.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -24,6 +24,9 @@ class Node(object):
     return self._left
 
   def SetLeft(self, node):
+    if not node:
+      return
+
     if node.data > self.data:
       raise ValueError(
           'Left child\'s value %d cannot be greater than root: %d' %
@@ -35,6 +38,9 @@ class Node(object):
     return self._right
 
   def SetRight(self, node):
+    if not node:
+      return
+
     if node.data < self.data:
       raise ValueError(
           'Right child\'s value %d cannot be less than the root: %d' %
@@ -47,7 +53,39 @@ class BinarySearchTree(object):
   def __init__(self):
     self.root = None
 
+  def get_data_inorder(self, current=None):
+    """Returns all the nodes' data in the tree in inorder.
+
+    Args:
+      current: Current processing node.
+
+    Returns:
+      List of integers.
+    """
+    if not current:
+      current = self.root
+
+    inorder = []
+    if current.Left():
+      inorder += self.get_data_inorder(current.Left())
+
+    inorder.append(current.data)
+
+    if current.Right():
+      inorder += self.get_data_inorder(current.Right())
+
+    return inorder
+
   def find(self, data, current=None):
+    """Finds and returns the node in the tree with the given data.
+
+    Args:
+      data: (int) Data on the required node.
+      current: (Node) Current processing node.
+
+    Returns:
+      Node with the given value or None if no such node is found.
+    """
     if not current:
       current = self.root
 
@@ -60,22 +98,92 @@ class BinarySearchTree(object):
     else:
       return None
 
-  def is_binary_search_tree(self, current=None, min=MIN_INT, max=MAX_INT):
+  def is_binary_search_tree(
+      self, current=None, minVal=MIN_INT, maxVal=MAX_INT):
+    """Determines if the tree is a true binary search tree.
+
+    For a tree to be a binary search tree, it must be:
+    1. A binary tree
+    2. The left child should be smaller than the current node.
+    3. The right child should be greater than the current node.
+    4. Every node in the left subtree should be smaller than the current node.
+    5. Every node in the right subtree should be smaller than the current node.
+
+    Simply checking the left and right child's value with the current node is
+    not enough since a left child can be smaller than the current node but the
+    left child's right child could be greater than its grand parent node.
+
+    One method of checking would be to recursively fetch left and right subtree
+    nodes for each element and compare data of nodes from both subtree with the
+    current node's data.
+
+    Second more efficient method would be to recursively give each node the
+    minimum and maximum range for values that it and its children can have.
+    The initial minimum and maximum value for the root node would be the the
+    maximum system integer limits.
+
+    A third simple way of doing it would be to do an in-order traversal of the
+    tree and put the results in an array. If the array is sorted than the tree
+    is a binary search tree.
+
+    Args:
+      current: Node to start processing from. (Node)
+      min: Minimum allowed value for all children of the current node. (int)
+      max: Maximum allowed value for all children of the current node. (int)
+
+    Returns:
+      True if the tree is a binary search tree else False. (bool)
+    """
+    # If no current node is given, start with root as the current node.
     if not current:
       current = self.root
 
-    if current.data < min or current.data > max:
+    # If the current node's data is out of bounds, it is not a BST.
+    if current.data < minVal or current.data > maxVal:
       return False
 
     is_left_bst = True
     if current.Left():
-      is_left_bst = self.is_binary_search_tree(current.Left(), min, current.data - 1)
+      # Give new bounds for the left subtree. The lower bound remains the same
+      # as the current subtree but the upper bound would be one less than the
+      # current node's value.
+      is_left_bst = self.is_binary_search_tree(current.Left(),
+                                               minVal, current.data - 1)
 
     is_right_bst = True
     if current.Right():
-      is_right_bst = self.is_binary_search_tree(current.Right(), current.data + 1, max)
+      # Give new bounds to the right subtree. The upper bound will be same as
+      # the current subtree but the lower bould would be one greater than the
+      # current node's value.
+      is_right_bst = self.is_binary_search_tree(current.Right(),
+                                                current.data + 1, maxVal)
 
+    # return true only if both left and right subtrees are BST.
     return is_left_bst and is_right_bst
+
+  def create_bst_from_preorder(self, preorder):
+    if not preorder:
+      return None
+
+    if len(preorder) == 1:
+      return Node(preorder[0])
+
+    current_node = Node(preorder[0])
+
+    preorder = preorder[1:]
+    sentinel = len(preorder)
+    for index in range(len(preorder)):
+      if preorder[index] > current_node.data:
+        sentinel = index
+        break
+
+    left_preorder = preorder[:sentinel]
+    right_preorder = preorder[sentinel:]
+
+    current_node.SetLeft(self.create_bst_from_preorder(left_preorder))
+    current_node.SetRight(self.create_bst_from_preorder(right_preorder))
+
+    return current_node
 
 def create_first_tree_and_assert():
   tree = BinarySearchTree()
@@ -105,10 +213,13 @@ def create_first_tree_and_assert():
   node_9 = Node(9)
   node_8.SetRight(node_9)
 
-  print(tree.find(1))
-  print(tree.find(10))
+  assert tree.find(1) is not None
+  assert tree.find(10) is None
 
   assert tree.is_binary_search_tree() is True
+
+  # This is sorted since the tree is a binary search tree.
+  assert tree.get_data_inorder() == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def create_second_tree_and_assert():
@@ -129,6 +240,14 @@ def create_second_tree_and_assert():
 
   assert tree.is_binary_search_tree() is False
 
+  # This is not sorted since it is not a binary search tree.
+  assert tree.get_data_inorder() == [1, 2, 4, 3, 5]
+
+
 if __name__ == '__main__':
   create_first_tree_and_assert()
   create_second_tree_and_assert()
+
+  tree = BinarySearchTree()
+  tree.root = tree.create_bst_from_preorder([10, 5, 1, 7, 40, 50])
+  assert tree.get_data_inorder() == [1, 5, 7, 10, 40, 50]
