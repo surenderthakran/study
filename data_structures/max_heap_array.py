5fcd2299558f2349a14a3e5ad40e62384e9de973
surenderthakran <surender.thakran89@gmail.com>
2018-06-30 10:49:04 +0530

    Updated docstrings and comments in the max heap implementation.

--- data_structures/max_heap_array.py
+++ data_structures/max_heap_array.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding:utf-8 -*-
 
+"""Implements a Max Binary Heap.
+
+Usage:
+$ python max_heap_array.py
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -21,6 +27,7 @@ class MaxHeap(object):
     return str(self.arr)
 
   def left_index(self, index):
+    """Returns left child's index of the given node if it exists else -1."""
     left_index = 2 * index + 1
     # Check if left index is out of bounds.
     if left_index > len(self.arr) - 1:
@@ -29,6 +36,7 @@ class MaxHeap(object):
     return left_index
 
   def left(self, index):
+    """Returns left child of the given node if t exists else None."""
     left_index = self.left_index(index)
 
     if left_index < 0:
@@ -37,6 +45,8 @@ class MaxHeap(object):
     return self.arr[left_index]
 
   def right_index(self, index):
+
+    """Returns right child's index of the given node if it exists else -1."""
     right_index = 2 * index + 2
     # Check if right index is out of bounds.
     if right_index > len(self.arr) - 1:
@@ -45,6 +55,7 @@ class MaxHeap(object):
     return right_index
 
   def right(self, index):
+    """Returns right child of the given node if it exists else None."""
     right_index = self.right_index(index)
 
     if right_index < 0:
@@ -53,6 +64,7 @@ class MaxHeap(object):
     return self.arr[right_index]
 
   def parent_index(self, index):
+    """Returns parent's index of the given node if it exists else -1."""
     parent_index = (index - 1) // 2
     # Check if parent index is out of bounds.
     if parent_index < 0:
@@ -61,6 +73,7 @@ class MaxHeap(object):
     return parent_index
 
   def parent(self, index):
+    """Returns parent of the given node if it exists else None."""
     parent_index = self.parent_index(index)
 
     if parent_index < 0:
@@ -69,13 +82,27 @@ class MaxHeap(object):
     return self.arr[parent_index]
 
   def heap_array(self):
+    """Returns the array representing the heap."""
     return self.arr
 
   def add(self, data):
+    """Adds a new element to the heap.
+
+    The new element is added as the last node in the last level i.e. at the end
+    of the heap's arrayn and then bubbled up to its proper position.
+
+    Args:
+      data: (int) Element to be added to the heap.
+    """
     self.arr.append(data)
     self.bubble_up_element()
 
   def bubble_up_element(self, index=None):
+    """Moves an element up the heap levels to its appropriate level.
+
+    Args:
+      index: (int) Index of the element in the heap array to be bubbled up.
+    """
     # If no index is given, heapify the last element.
     if not index:
       index = len(self.arr) - 1
@@ -91,27 +118,53 @@ class MaxHeap(object):
       index = self.parent_index(index)
 
   def remove(self):
+    """Removes and returns the root element from the heap.
+
+    Returns:
+      The root element in the heap.
+    """
+    # Locate the root element.
     top = self.arr[0]
+
+    # Replace the root with the last elemrnt.
     self.arr[0] = self.arr[len(self.arr) - 1]
+
+    # Remove the last element from the array.
     self.arr = self.arr[:-1]
 
+    # Sink down the new value at the root to its proper position.
     self.sink_down_element(0)
 
     return top
 
   def sink_down_element(self, index):
+    """Sinks down the given element to its proper position in the max heap.
+
+    Amongst the parent and its two children, we deduce the largest element,
+    and if the parent is not the largest we replace it with the largest child.
+    Repeat the same process with the replaced child.
+
+    Args:
+      index: (int) Index of the element the array to be sunk down.
+    """
+    # Start with setting parent as the largest.
     largest = index
+
+    # If left exists and is larger than the largest, set it's index as largest.
     if self.left(index) and self.left(index) > self.arr[largest]:
       largest = self.left_index(index)
 
+    # If right exists and is larger than the largest, set it as largest. 
     if self.right(index) and self.right(index) > self.arr[largest]:
       largest = self.right_index(index)
 
+    # If parent is not the largest, swap it with the largest.
     if largest != index:
       temp = self.arr[index]
       self.arr[index] = self.arr[largest]
       self.arr[largest] = temp
 
+      # Repeat with the swapped child as parent.
       self.sink_down_element(largest)
 
   def get_data_preorder(self, current=None):
@@ -137,7 +190,7 @@ class MaxHeap(object):
     return data
 
 
-def create_first_heap_and_assert():
+def create_heap_and_assert():
   heap = MaxHeap([35, 33, 42, 10, 14, 19, 27, 44, 26, 31])
 
   assert heap.heap_array() == [44, 42, 35, 33, 31, 19, 27, 10, 26, 14]
@@ -150,4 +203,4 @@ def create_first_heap_and_assert():
 
 
 if __name__ == '__main__':
-  create_first_heap_and_assert()
+  create_heap_and_assert()
