7bc108ee75de67cfef498ee9212bc6378e13cb0f
surenderthakran <surender.thakran89@gmail.com>
2018-07-13 16:12:48 +0530

    Updated README.md to include problem to track median in alive array.

--- problems/track_median.py
+++ problems/track_median.py
@@ -374,6 +374,7 @@ def track_median(element, left_heap, right_heap, median):
 
 
 def test_track_median():
+  """Function to test median tracking logic."""
   arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
   # Left heap is a Max Heap.
