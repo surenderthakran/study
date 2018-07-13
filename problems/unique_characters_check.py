faad0a02d977812ae30f1f0fa2006c98bfb0eeaf
surenderthakran <surender.thakran89@gmail.com>
2018-07-13 19:07:26 +0530

    Started on problem to check if all characters in a string are unique.

---
+++ problems/unique_characters_check.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""Check if a string has all unique characters.
+Without using any additional data structures.
+
+Usage:
+$ python unique_characters_check.py
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def is_unique_chars(str):
+  """Checks if the given string has all unique characters.
+
+  Args:
+    str: (string)
+
+  Returns:
+    True if all characters are unique else False.
+  """
+  pass
+
+
+if __name__ == '__main__':
+  assert is_unique_chars('abcdef') is True
