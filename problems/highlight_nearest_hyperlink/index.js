bfd5b09b48c8d539b3f0d5a0eeb1c292fcddc66b
surenderthakran <surender.thakran89@gmail.com>
2018-10-19 18:51:13 +0530

    Updated problem about highlighting nearest hyperlink to the mouse cursor.

--- problems/highlight_nearest_hyperlink/index.js
+++ problems/highlight_nearest_hyperlink/index.js
@@ -4,10 +4,17 @@
   window.addEventListener('load', () => {
     console.log('window loaded');
 
-    document.addEventListener('mousemove', (event) => {
-      console.log('mouse moved to:', event.clientX, event.clientY);
-    });
+    document.addEventListener('mousemove', throttleMouseMoveEvents(200, mouseMoveHandler));
   });
+
+  function throttleMouseMoveEvents(delay, handler) {
+    let lastCalled;
+    return handler;
+  }
+
+  function mouseMoveHandler(event) {
+    console.log('mouse moved to:', event.clientX, event.clientY);
+  }
 
   class Hyperlink {
     constructor (element) {
