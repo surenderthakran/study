93c862b4b2929edef3faeed5164611163c293dd2
surenderthakran <surender.thakran89@gmail.com>
2018-10-22 12:54:20 +0530

    Updated problem to highlight hyperlink nearest to the mouse cursor to throttle movemove events.

--- problems/highlight_nearest_hyperlink/index.js
+++ problems/highlight_nearest_hyperlink/index.js
@@ -1,19 +1,21 @@
 'use strict';
 
 (() => {
+  let lastMousemoveEventHandleTime = 0;
+  const mousemoveThrottleThreshold = 200;
+
   window.addEventListener('load', () => {
     console.log('window loaded');
 
-    document.addEventListener('mousemove', throttleMouseMoveEvents(200, mouseMoveHandler));
+    document.addEventListener('mousemove', mouseMoveHandler);
   });
 
-  function throttleMouseMoveEvents(delay, handler) {
-    let lastCalled;
-    return handler;
-  }
-
   function mouseMoveHandler(event) {
-    console.log('mouse moved to:', event.clientX, event.clientY);
+    let eventTime = new Date().getTime();
+    if (eventTime - lastMousemoveEventHandleTime > mousemoveThrottleThreshold) {
+      console.log('mouse moved to:', event.clientX, event.clientY);
+      lastMousemoveEventHandleTime = eventTime;
+    }
   }
 
   class Hyperlink {
