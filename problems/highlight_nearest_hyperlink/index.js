da07e3c701e3e94845cc1b305964fa23e7e5d07e
surenderthakran <surender.thakran89@gmail.com>
2018-10-22 16:12:45 +0530

    Updated problem to highlight hyperlink nearest to the mouse cursor and updated README.md

--- problems/highlight_nearest_hyperlink/index.js
+++ problems/highlight_nearest_hyperlink/index.js
@@ -1,13 +1,29 @@
 'use strict';
 
 (() => {
+  /**
+   * List of hyperlinks in the page.
+   */
   let hyperlinks = [];
 
+  /**
+   * Time since mousemove event was handled last.
+   */
   let lastMousemoveEventHandleTime = 0;
+
+  /**
+   * Minimum time between two mousemove event handlings.
+   */
   const mousemoveThrottleThreshold = 200;
 
+  /**
+   * Currently highlighted hyperlink.
+   */
   let highlightedHyperlink;
 
+  /**
+   * Class representing an Euclidean point.
+   */
   class Point {
     constructor (x, y) {
       this.x = x;
@@ -15,6 +31,9 @@
     }
   }
 
+  /**
+   * Class representing an Euclidean rectangle.
+   */
   class Rectangle {
     constructor(topLeft, height, width) {
       this.topLeft = topLeft;
@@ -23,42 +42,65 @@
     }
   }
 
+  /**
+   * Set listener for when all page contents are loaded.
+   */
   window.addEventListener('load', () => {
-    scanPageHyperlinks();
-
-    document.addEventListener('mousemove', mouseMoveHandler);
-  });
-
-  function scanPageHyperlinks() {
+    // Identify all hyperlinks in the page.
     for (let anchor of document.getElementsByTagName('a')) {
       hyperlinks.push(anchor);
     }
-  }
 
+    // Set listener on mousemove events.
+    document.addEventListener('mousemove', mouseMoveHandler);
+  });
+
+  /**
+   * Function handling mosemove events.
+   * @param {!MouseEvent} event - mousemove event object.
+   */
   function mouseMoveHandler(event) {
+    // Current timestamp.
     let eventTime = new Date().getTime();
+
+    // If current timestamp is after throttling threshold has expired.
     if (eventTime - lastMousemoveEventHandleTime > mousemoveThrottleThreshold) {
       lastMousemoveEventHandleTime = eventTime;
 
+      // Find nearest hyperlink to the mouse cursor.
       let nearestHyperlink = findNearestHyperlink(new Point(event.clientX,
                                                             event.clientY));
 
-      highlightHyperlink(nearestHyperlink);
+      if (nearestHyperlink) {
+        // Highlight nearest hyperlink.
+        highlightHyperlink(nearestHyperlink);
+      }
     }
   }
 
+  /**
+   * Function to find nearest hyperlink to the given point.
+   * @param {!Point} point
+   *
+   * @return {?HTMLAnchorElement}
+   */
   function findNearestHyperlink(point) {
     let shortestDistance;
     let nearestHyperlink;
 
+    // Iterate over list of all hyperlinks.
     for (let hyperlink of hyperlinks) {
+      // Identify current bounds of the hyperlink.
       let boundingClientRect = hyperlink.getBoundingClientRect();
       let hyperlinkRect = new Rectangle(
           new Point(boundingClientRect.x, boundingClientRect.y),
           boundingClientRect.height,
           boundingClientRect.width);
 
+      // Calculate shortest distance between the point and the rectangle.
       let distance = distanceBetweenRectangleAndPoint(hyperlinkRect, point);
+
+      // If distance is the shortest till now, mark the hyperlink as nearest.
       if (!shortestDistance || distance < shortestDistance) {
         shortestDistance = distance;
         nearestHyperlink = hyperlink;
@@ -69,36 +111,53 @@
   }
 
   /**
+   * Function to find distance between a rectangle and a point.
+   * The space around the rectangle (section V) is divided into several
+   * sections.
+   *
    *   I   |   II   | III
    *  -----+--------+-----
    *   IV  |   V    | VI
    *  -----+--------+-----
    *   VII |  VIII  | IX
+   *
+   * @param {!Rectangle} rectangle
+   * @param {!Point} point
+   *
+   * @return {number}
    */
   function distanceBetweenRectangleAndPoint(rectangle, point) {
     let distance;
-    if (point.x < rectangle.topLeft.x) {  // Quadrants I, IV or VII
-      if (point.y < rectangle.topLeft.y) {  // Quadrant I
+
+    if (point.x < rectangle.topLeft.x) {  // Sections I, IV or VII
+      if (point.y < rectangle.topLeft.y) {  // Section I
         distance = distanceBetweenPoints(point, rectangle.topLeft);
-      } else if (point.y > (rectangle.topLeft.y + rectangle.height)) {  // Quadrant VII
-        distance = distanceBetweenPoints(point, new Point(rectangle.topLeft.x, rectangle.topLeft.y + rectangle.height));
-      } else {  // Quadrant IV
+      } else if (point.y > (rectangle.topLeft.y + rectangle.height)) {  // Section VII
+        distance = distanceBetweenPoints(
+            point, new Point(rectangle.topLeft.x,
+                             rectangle.topLeft.y + rectangle.height));
+      } else {  // Section IV
         distance = rectangle.topLeft.x - point.x;
       }
-    } else if (point.x > (rectangle.topLeft.x + rectangle.width)) { // Quadrants III, VI or IX
-      if (point.y < rectangle.topLeft.y) {  // Quadrant III
-        distance = distanceBetweenPoints(point, new Point(rectangle.topLeft.x + rectangle.width, rectangle.topLeft.y));
-      } else if (point.y > (rectangle.topLeft.y + rectangle.height)) {  // Quadrant IX
-        distance = distanceBetweenPoints(point, new Point(rectangle.topLeft.x + rectangle.width, rectangle.topLeft.y + rectangle.height));
-      } else {  // Quadrant VI
+    } else if (point.x > (rectangle.topLeft.x + rectangle.width)) { // Sections III, VI or IX
+      if (point.y < rectangle.topLeft.y) {  // Section III
+        distance = distanceBetweenPoints(
+            point, new Point(rectangle.topLeft.x + rectangle.width,
+                             rectangle.topLeft.y));
+      } else if (
+          point.y > (rectangle.topLeft.y + rectangle.height)) {  // Section IX
+        distance = distanceBetweenPoints(
+            point, new Point(rectangle.topLeft.x + rectangle.width,
+                             rectangle.topLeft.y + rectangle.height));
+      } else {  // Section VI
         distance = point.x - (rectangle.topLeft.x + rectangle.width);
       }
-    } else {  // Quadrants II, V or VIII
-      if (point.y < rectangle.topLeft.y) {  // Quadrant II
+    } else {  // Sections II, V or VIII
+      if (point.y < rectangle.topLeft.y) {  // Section II
         distance = rectangle.topLeft.y - point.y;
-      } else if (point.y > (rectangle.topLeft.y + rectangle.height)) {  // Quadrant IX
+      } else if (point.y > (rectangle.topLeft.y + rectangle.height)) {  // Section IX
         distance = point.y - (rectangle.topLeft.y + rectangle.height);
-      } else {  // Quadrant VI
+      } else {  // Section VI
         distance = 0;
       }
     }
@@ -106,10 +165,21 @@
     return distance;
   }
 
+  /**
+   * Function to find distance between two points.
+   * @param {!Point} p1
+   * @param {!Point} p2
+   *
+   * @return {number}
+   */
   function distanceBetweenPoints(p1, p2) {
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 
+  /**
+   * Function to highlight and unhighlight hyperlinks.
+   * @param {!HTMLAnchorElement} hyperlink
+   */
   function highlightHyperlink(hyperlink) {
     if (highlightedHyperlink) {
       highlightedHyperlink.style.backgroundColor = 'white';
