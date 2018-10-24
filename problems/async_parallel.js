d52c42eda18c6abd4d2859d042260c083c1dfa87
surenderthakran <surender.thakran89@gmail.com>
2018-10-24 13:34:19 +0530

    Updated problem: Implement async.parallel javascript function.

--- problems/async_parallel.js
+++ problems/async_parallel.js
@@ -1,23 +1,55 @@
 'use strict';
 
 (function() {
+
 	class Async {
+
     parallel(tasks, callback) {
       const response = [];
-      callback(response);
+      let finished = 0;
+
+      const interval = setInterval(() => {
+        if (finished === tasks.length) {
+          clearInterval(interval);
+          return callback(response);
+        }
+      }, 200);
+
+      for (let i = 0, len = tasks.length; i < len; i++) {
+        tasks[i]((err, result) => {
+          console.log(result);
+          finished++;
+          response[i] = result;
+        });
+      }
     }
   }
 
-	function asyncTask(name, cb) {
-	  setTimeout(() => {
-	    cb(null, 'task ' + name + ' done!');
-	  }, Math.random() * 1000);
-	}
-
-  const tasks = [];
+  const tasks = [
+    function(cb) {
+      setTimeout(() => {
+        cb(null, 1);
+      }, Math.random() * 1000);
+    },
+    function(cb) {
+      setTimeout(() => {
+        cb(null, 2);
+      }, Math.random() * 1000);
+    },
+    function(cb) {
+      setTimeout(() => {
+        cb(null, 3);
+      }, Math.random() * 1000);
+    },
+    function(cb) {
+      setTimeout(() => {
+        cb(null, 4);
+      }, Math.random() * 1000);
+    },
+  ];
 
   const async = new Async();
   async.parallel(tasks, (result) => {
     console.log(result);
-  })
+  });
 })();
